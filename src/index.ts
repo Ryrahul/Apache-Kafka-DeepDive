08bc884d699ef0b4c7b3092cf0fb02f1d7388910
Rahul <caulirahul6@gmail.com>
2023-12-29 01:37:02 +0530

    feat: env

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,7 @@
+require("dotenv").config();
 import { Kafka } from "kafkajs";
-
-import * as ip from "ip";
-
-const PORT = 9092;
-
 export const kafka = new Kafka({
   clientId: "my-app",
-  brokers: ["172.20.254.174:9092"],
+  brokers: [`${process.env.IP}:${process.env.PORT}`],
 });
+async function runService() {}
